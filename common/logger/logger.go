3776dc92947eb144c7feece4a2a2e148a59a0f77
raul <raulandrei2017@yahoo.com>
2025-03-12 17:41:45 +0200

    common/logger: add initializer function for zap logger to redirect output to app.log

---
+++ common/logger/logger.go
@@ -0,0 +1,26 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+func InitLogger() *zap.Logger {
+	// Configure Zap logger
+	config := zap.NewProductionConfig()
+	logFile, err := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
+	}
+	config.OutputPaths = []string{logFile.Name()}
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+
+	// Initialize logger
+	l, err := config.Build()
+	if err != nil {
+		panic(err)
+	}
+	return l
+}
