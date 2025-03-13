a4d44b61c6b496ffa7f7b0a6a94ea346ef4a9fca
raul <raulandrei2017@yahoo.com>
2025-03-13 20:32:28 +0200

    chore: add go library to support .env files

--- indexer/cmd/main.go
+++ indexer/cmd/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"github.com/joho/godotenv"
+	"log"
 )
 
 func main() {
-	var s = ""
-	fmt.Printf(s[:min(len(s), 200)])
+	// Env file setup
+	err := godotenv.Load("./../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 }
